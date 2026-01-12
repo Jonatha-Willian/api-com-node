c8c9e40e969c6ab549151ed351e7bc4e76f83ea4
Jonatha Willian P Santos <jonathasantos.dev@hptmail.com>
2026-01-12 10:52:04 -0300

    att 0.2

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,15 @@
 import express from 'express';
 import helmet from 'helmet';
 import router from './routes';
+import passport from 'passport';
 
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
+
+server.use(passport.initialize())
 
 server.use('/', router);
 
